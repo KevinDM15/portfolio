0dfd7b7d67f345b1d08f6a474365c41236fd1777
kevindm15 <kevindiazm.14@hotmail.com>
2026-05-18 12:44:32 -0500

    fix: add Vercel adapter for server-rendered API route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import unocss from '@unocss/astro';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel(),
 	integrations: [
 		react(),
 		unocss({
